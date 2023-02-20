956c4b456c9239f17fe6c8a4b92b627214c7e317
David Santos <dfps.gestao@gmail.com>
2023-02-20 01:10:27 +0000

    repeatString.js

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,6 +1,32 @@
-const repeatString = function() {
+
+
+function repeatString(string, num) {
+  if (num < 0) {
+    return "ERROR";
+  }
+    let baseString ="";
+    while (num > 0){
+      baseString += string;
+      num--;
+    }
+    return baseString;
 
 };
+
+
+let stringThree = repeatString("hey", 3);
+let stringTen = repeatString("hey", 10);
+let stringOne = repeatString("hey", 1);
+let stringZero = repeatString("hey", 0);
+let stringMinusOne = repeatString("hey", -1);
+let stringRandom = repeatString("hey", Math.floor(Math.random() * 30));
+let stringBlank = repeatString ("", 10);
+
+
+
+
+
+
 
 // Do not edit below this line
 module.exports = repeatString;
