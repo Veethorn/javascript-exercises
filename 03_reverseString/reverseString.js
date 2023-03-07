4795c4ff15b0181edbab14702f5b98288c224558
David Santos <dfps.gestao@gmail.com>
2023-03-07 11:59:58 +0000

    reverseString.g.js

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,6 +1,21 @@
-const reverseString = function() {
+function reverseString (string) { 
+    return string.split("").reverse().join().replace(/,/g,"");
+   }
+   
 
-};
+ let x = reverseString ("hello");
+
+ let y = reverseString ("hello there");
+
+ let z = reverseString ("123! abc!");
+
+ let w = reverseString ("");
+
+/*   let stringOne = reverseString("hello");
+   let stringSplit = stringOne.split("");
+   let stringReverse = stringSplit.reverse();
+   let stringJoin = stringReverse.join();
+   let stringFull = stringJoin.replace(/,/g,""); */
 
 // Do not edit below this line
 module.exports = reverseString;
